da67446315346e08aaa9923ac15eb4bd4fbec958
johannes <jmuljana@gmail.com>
2018-12-20 04:51:07 +0000

    adding validator class

---
+++ classes/Account.php
@@ -0,0 +1,11 @@
+<?php
+namespace aitsyd;
+class Account extends Database{
+  public function __construct(){
+    parent::__construct();
+  }
+  public function signUp($username,$email,$password){
+    
+  }
+}
+?>
