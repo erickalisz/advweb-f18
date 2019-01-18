c8c3ea7f00936ca29f8a66e894d72157fb441792
johannes <jmuljana@gmail.com>
2019-01-18 07:54:24 +0000

    updated navigation code

--- includes/navigation.inc.php
+++ includes/navigation.inc.php
@@ -6,4 +6,15 @@ $nav = new Navigation();
 $pages = $nav -> pages;
 
 $currentPage = Page::getName();
+
+if( isset($_SESSION['email']) ){
+  $user = array();
+  $user['email'] = $_SESSION['email'];
+  $user['username'] = $_SESSION['username'];
+  $user['account_id'] = $_SESSION['account_id'];
+}
+else{
+  $user = null;
+}
+
 ?>
